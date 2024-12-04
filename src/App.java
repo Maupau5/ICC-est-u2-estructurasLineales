153d94055fb50ed569601ee2ddd41970b3e9e938
Maupau5 <mauroipaul@gmail.com>
2024-12-04 12:30:21 -0500

    Estrcuturas Lineales

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import Materia.Stacks.Stack;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        runStack();
+    }
+
+    public static void runStack() {
+        // Instanciar la clase
+        Stack stack = new Stack();
+
+        // Agregar elementos a la fila.
+        stack.push(5);
+        stack.push(7);
+        stack.push(10);
+        stack.push(20);
+
+        // Mostrar los valores.
+        System.out.println("Cima   -> "+stack.peek());
+        System.out.println("Retirar-> "+stack.pop());
+        System.out.println("Cima   -> "+stack.peek());
+        System.out.println("Retirar-> "+stack.pop());
+        System.out.println("Cima   -> "+stack.peek());
+        System.out.println("Retirar-> "+stack.pop());
+        System.out.println("Cima   -> "+stack.peek());
+        System.out.println("Retirar-> "+stack.pop());
+        
     }
 }
