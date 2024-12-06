f5295aa63dd1c8d4000e0104bfab89cc40ad5deb
Maupau5 <mauroipaul@gmail.com>
2024-12-06 12:13:24 -0500

    Estructuras Lineales

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 import Materia.Stacks.Stack;
+import Materia.Stacks.StackGeneric;
+import Models.Pantalla;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        runStack();
+        //runStack();
+        runStackGeneric();
     }
 
     public static void runStack() {
@@ -24,5 +27,27 @@ public class App {
         System.out.println("Retirar-> "+stack.pop());
         System.out.println("Cima   -> "+stack.peek());
         System.out.println("Retirar-> "+stack.pop());
+    }
+
+    public static void runStackGeneric() {
+        // Instanciar la clase
+        StackGeneric<Pantalla> router = new StackGeneric<>();
+
+        // Agregar elementos a la fila.
+        router.push(new Pantalla(1, "Home Page", "/home"));
+        router.push(new Pantalla(1, "Menu Page", "/home/menu"));
+        router.push(new Pantalla(1, "Users Page", "/home/menu/usuarios"));
+
+        // Mostrar los valores.
+        System.out.println("Estoy en "+router.peek().getRuta());
+        System.out.println("Regreso a "+router.popNode().getNext().getValue().getRuta());
+        System.out.println("Estoy en "+router.peek().getRuta());
+
+        router.push(new Pantalla(1, "Settings Page", "/home/menu/settings"));
+        System.out.println("Pantallas = "+ router.getSize());
+        System.out.println("Estoy en "+router.peek().getRuta());
+        
+        router.printStack();
+        
     }
 }
---
+++ src/Materia/Stacks/StackGeneric.java
@@ -0,0 +1,72 @@
+package Materia.Stacks;
+
+import java.util.EmptyStackException;
+
+import Materia.Models.NodeGeneric;
+
+public class StackGeneric<T> {
+    private NodeGeneric<T> top;
+    private int size; // Variable del tamanño O(1)
+
+    // Crea una fila instanciando el nodo top en null y el tamaño en 0
+    public StackGeneric() {
+        this.top = null;
+        this.size = 0;
+    }
+
+    public void push(T value) {
+        NodeGeneric<T> newNode = new NodeGeneric<T>(value);
+        newNode.setNext(top);
+        top = newNode;
+        size++;
+
+    }
+
+    public T pop() {
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        T value = top.getValue();
+        top = top.getNext();
+        size--;
+        return value;
+    }
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public NodeGeneric<T> popNode() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        NodeGeneric<T> value = top;
+        top = top.getNext();
+        size--;
+        return value;
+    }
+
+    public T peek() {
+        if(isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.getValue();
+    }
+
+    public void printStack() {
+        NodeGeneric<T> current = top;
+        while(current != null) {
+            System.out.println(current.getValue());
+            current = current.getNext();
+        }
+        
+    }
+    public int getSize() {
+        int size = 0;
+        NodeGeneric<T> current = top;
+        while(current != null) {
+            size++;
+            current = current.getNext();
+        }
+        return size;
+    }
+}
